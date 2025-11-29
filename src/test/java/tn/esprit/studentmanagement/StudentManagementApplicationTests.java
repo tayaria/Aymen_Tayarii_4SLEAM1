36978184fda8cf6575f7ca3ce42a96dd954e3e7b
tayaria <aymen.tayari@esprit.tn>
2025-11-29 21:50:25 +0100

    ajoute test unitaire

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -3,6 +3,7 @@ package tn.esprit.studentmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -10,6 +11,14 @@ class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+
+    @Test
+    void simpleTest() {
+        int a = 2 + 2;
+        assertEquals(4, a, "2 + 2 doit faire 4");
     }
 
 }
