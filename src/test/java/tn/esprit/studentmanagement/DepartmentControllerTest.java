b3624f9b07a3e8765a0850ef7c34b13872b91f5a
tayaria <aymen.tayari@esprit.tn>
2025-12-02 20:24:26 +0100

    add testPlusUnitaire

--- src/test/java/tn/esprit/studentmanagement/DepartmentControllerTest.java
+++ src/test/java/tn/esprit/studentmanagement/DepartmentControllerTest.java
@@ -1,13 +1,10 @@
 package tn.esprit.studentmanagement;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-
+import org.mockito.MockitoAnnotations;
 import tn.esprit.studentmanagement.controllers.DepartmentController;
 import tn.esprit.studentmanagement.entities.Department;
 import tn.esprit.studentmanagement.services.IDepartmentService;
@@ -16,7 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-@ExtendWith(MockitoExtension.class)
+import static org.mockito.Mockito.*;
 
 class DepartmentControllerTest {
 
@@ -26,25 +23,58 @@ class DepartmentControllerTest {
     @InjectMocks
     private DepartmentController departmentController;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    void testGetAllDepartments() {
-        Department dep1 = new Department();
-        dep1.setIdDepartment(1L);
-        dep1.setName("IT");
-        dep1.setHead("head1");
-        dep1.setLocation("test1");
-        dep1.setPhone("55455455");
-        Department dep2 = new Department();
-        dep2.setIdDepartment(2L);
-        dep2.setName("HR");
-        dep2.setHead("head1");
-        dep2.setLocation("test1");
-        dep2.setPhone("55455455");
+    void testGetAllDepartment() {
+        Department dep1 = new Department(); dep1.setIdDepartment(1L); dep1.setName("IT");
+        Department dep2 = new Department(); dep2.setIdDepartment(2L); dep2.setName("HR");
 
-
-        Mockito.when(departmentService.getAllDepartments()).thenReturn(Arrays.asList(dep1, dep2));
+        when(departmentService.getAllDepartments()).thenReturn(Arrays.asList(dep1, dep2));
 
         List<Department> result = departmentController.getAllDepartment();
         assertEquals(2, result.size());
+        verify(departmentService, times(1)).getAllDepartments();
+    }
+
+    @Test
+    void testGetDepartmentById() {
+        Department dep = new Department(); dep.setIdDepartment(1L); dep.setName("IT");
+        when(departmentService.getDepartmentById(1L)).thenReturn(dep);
+
+        Department result = departmentController.getDepartment(1L);
+        assertEquals("IT", result.getName());
+        verify(departmentService, times(1)).getDepartmentById(1L);
+    }
+
+    @Test
+    void testCreateDepartment() {
+        Department dep = new Department(); dep.setName("Finance");
+        when(departmentService.saveDepartment(dep)).thenReturn(dep);
+
+        Department result = departmentController.createDepartment(dep);
+        assertEquals("Finance", result.getName());
+        verify(departmentService, times(1)).saveDepartment(dep);
+    }
+
+    @Test
+    void testUpdateDepartment() {
+        Department dep = new Department(); dep.setName("UpdatedDept");
+        when(departmentService.saveDepartment(dep)).thenReturn(dep);
+
+        Department result = departmentController.updateDepartment(dep);
+        assertEquals("UpdatedDept", result.getName());
+        verify(departmentService, times(1)).saveDepartment(dep);
+    }
+
+    @Test
+    void testDeleteDepartment() {
+        doNothing().when(departmentService).deleteDepartment(1L);
+
+        departmentController.deleteDepartment(1L);
+        verify(departmentService, times(1)).deleteDepartment(1L);
     }
 }
